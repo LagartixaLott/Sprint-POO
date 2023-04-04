592ee1f3da300bca9c941a424163bb986d209ea0
LagartixaLott <127571086+LagartixaLott@users.noreply.github.com>
2023-04-04 16:38:39 -0300

    Add files via upload

--- Aeronave.php
+++ Aeronave.php
@@ -2,78 +2,112 @@
 
 include_once("CompanhiaAerea.php");
 
+class Excecao_Resgistro_aviao{
+ public  string $registro_erro;
+}
+
 class Aeronave{
 
-private{
-    string $fabricante;
-    string $modelo;
-    float $carga;
-    int $pasageiros;
-    string $registro;
-    CompanhiaAerea $CompanhiaAerea_;
 
+ protected  string $fabricante;
+ protected string $modelo;
+ protected  float $carga;
+ protected   int $pasageiros;
+ protected   string $registro;
+ protected   CompanhiaAerea $CompanhiaAerea_;
+
+
+
+public function cadastro_aeronave ($fabricante_f,$modelo_f,$carga_f,$passageiros_f,$registro_f,$companhiaAerea_f){
+public Aeronave $a;
+$a.set_fabricante($fabricante_f);
+$a.set_modelo($modelo_f);
+$a.set_carga($carga_f);
+$a.set_passageiro($passageiros_f);
+$a.set_registro($registro_f);
+$a.set_companhia($companhiaAerea_f);
 }
 
-cadastro_aeronave ($fabricante_f,$modelo_f,$carga_f,$passageiros_f,$registro_f,$companhiaAerea_f){
-
-
+public function validar_registro($registro_f){
+if(sizeof($registro_f)!=6){
+   public Excecao_Resgistro_aviao $e.$registro_erro=$registro_f;
+     throw new Exception("Error Processing Request", $e);
+    
+}
+elseif($registro_f[1]!="P"){
+   public Excecao_Resgistro_aviao $e.$registro_erro=$registro_f;
+     throw new Exception("Error Processing Request", $e);
+    
 }
 
-validar_registro($registro_f){
-
+elseif($registro_f[3]!="-"){
+   public  Excecao_Resgistro_aviao $e.$registro_erro=$registro_f;
+    throw new Exception("Error Processing Request", $e);
+    
 }
-
-get_fabricante(){
-
+elseif($registro_f[2]!=("P" || "S" || "R" || "T")){
+    Excecao_Resgistro_aviao $e.$registro_erro=$registro_f;
+    throw new Exception("Error Processing Request", $e);
+    
 }
-
-get_modelo(){
-  
-}
-
-get_carga(){
-
-
-}
-
-get_passageiro(){
-
-
-}
-
-get_registro(){
-
-
-}
-
-get_companhia_aerea(){
-
-
-}
-
-set_fabricante($fabricante_f){
-
-}
-
-set_modelo($modelo_f){
-
-}
-
-set_carga($carga_f){
-
-}
-
-set_passageiro($passageiro_f){
 
 
 }
 
-set_registro($registro_f){
+public function get_fabricante(){
+return $this->$fabricante;
+}
 
+public function get_modelo(){
+return $this->$modelo;
+}
+
+public function get_carga(){
+return $this->$carga;
 
 }
 
-set_companhia(CompanhiaAerea $companhia_aerea_f){
+public function get_passageiro(){
+return $this->$pasageiros;
+}
+
+public function get_registro(){
+return $this->$registro;
+
+}
+
+public function get_companhia_aerea(){
+return $this->$CompanhiaAerea_;
+
+}
+
+public function set_fabricante($fabricante_f){
+$this->$fabricante = $fabricante_f;
+}
+
+public function set_modelo($modelo_f){
+$this->$modelo=$modelo_f;
+}
+
+public function set_carga($carga_f){
+$this->$carga=$carga_f;
+}
+
+public function set_passageiro($passageiro_f){
+$this->$pasageiros=$passageiro_f;
+}
+
+public function set_registro($registro_f){
+try{
+validar_registro($registro_f);
+$this->registro=$registro_f;
+}catch(Excecao_Resgistro_aviao){
+    echo "registro invalido";
+}
+}
+
+public function set_companhia(CompanhiaAerea $companhia_aerea_f){
+$this->$CompanhiaAerea=$companhia_aerea_f;
 }
 
 };
