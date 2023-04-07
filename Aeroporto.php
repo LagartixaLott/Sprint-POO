86d5809de19d86f4e946fdf7c09d4e3cf30d734a
BrunaFaria <bruna.faria283@gmail.com>
2023-04-07 14:39:56 -0300

    definição inicial classe aeroporto

--- Aeroporto.php
+++ Aeroporto.php
@@ -16,42 +16,40 @@ class Aeroporto {
   }
 
   public function validar_sigla_aero(string $sigla_s){
-    throw new Exception("Sigla Inválida", 1);
-  
+    if(ctype_alpha($sigla_s) && strlen($sigla_s)==3) return true;
+    else return false;
   }
 
   public function get_sigla_aero(){
-  
+    return $this->sigla;
   }
 
   public function get_cidade(){
- 
+    return $this->cidade;
   }
 
   public function get_estado(){
-  
+    return $this->estado;
   }
 
   public function get_nome_aero(){
-
+    return $this->nome;
   }  
 
   public function set_sigla_aero(string $sigla_f){
-
+    $this->sigla = $sigla_f;
   }
 
   public function set_cidade(string $cidade_f){
-
-
+    $this->cidade = $cidade_f;
   }
 
   public function set_estado(string $estado_f){
-
-
+    $this->estado = $estado_f;
   } 
 
   public function set_nome(string $nome_f){
-
+    $this->nome = $nome_f;
   }
 
 };
