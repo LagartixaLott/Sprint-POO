c09d0c6507ada8d2ed8e4e359def7f645854bc4c
BrunaFaria <bruna.faria283@gmail.com>
2023-04-07 08:16:57 -0300

    primeiras alterações no aeroporto

--- Aeroporto.php
+++ Aeroporto.php
@@ -1,54 +1,57 @@
 <?php
 
 class Aeroporto {
-private{
-string $sigla;
-string $cidade;
-string $estado;
-string $nome;
-}
-cadastro_aeroporto(string $sigla_f,string $cidade_f,string $estado_f,string $nome_f){
 
-  //  
-}
-
-validar_sigla_aero(string $sigla_s){
-    //
-}
-
-get_sigla_aero(){
-
-}
-
-get_cidade(){
-
-}
-
-get_estado(){
+  protected string $sigla;
+  protected string $cidade;
+  protected string $estado;
+  protected string $nome;
 
 
-}
+  public function cadastro_aeroporto(string $sigla_f,string $cidade_f,string $estado_f,string $nome_f){
+    $this->sigla = $sigla_f;
+    $this->cidade = $cidade_f;
+    $this->estado = $estado_f;
+    $this->nome = $nome_f;
+  }
 
-get_nome_aero(){
+  public function validar_sigla_aero(string $sigla_s){
+    throw new Exception("Sigla Inválida", 1);
+  
+  }
 
-}
+  public function get_sigla_aero(){
+  
+  }
 
-set_sigla_aero(string $sigla_f){
+  public function get_cidade(){
+ 
+  }
 
-}
+  public function get_estado(){
+  
+  }
 
-set_cidade(string $cidade_f){
+  public function get_nome_aero(){
+
+  }  
+
+  public function set_sigla_aero(string $sigla_f){
+
+  }
+
+  public function set_cidade(string $cidade_f){
 
 
-}
+  }
 
-set_estado(string $estado_f){
-
-
-}
-
-set_nome(string $nome_f)
+  public function set_estado(string $estado_f){
 
 
+  } 
+
+  public function set_nome(string $nome_f){
+
+  }
 
 };
