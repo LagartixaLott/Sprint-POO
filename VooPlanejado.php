f9455ef17676ecc2601a26d308e33a738acd1e43
itzvini <viniciusmc.2109@gmail.com>
2023-04-08 11:21:34 -0300

    envio voo planejado

--- VooPlanejado.php
+++ VooPlanejado.php
@@ -7,71 +7,79 @@ include_once("CompanhiaAerea.php");
 enum Frequencia{
 
 }
+#array para guardar os voos planejado
 
+class VooPlanejado{
 
-class VooPlanejado extends DadosVoos{
+protected string $codigo;
+protected Aeroporto $Aeroporto_origem;
+protected Aeroporto $Aeroporto_destino;
+protected DateTime $hora_agendada_chegada;
+protected DateTime $hora_agendada_saida;
+protected Aeronave $Aviao_esperado;
+protected frequencia $Frequencia_voo;
+public static array $historico_planejado = [];    
 
-private{
-    string $codigo;
-    Aeroporto $Aeroporto_origem;
-    Aeroporto $Aeroporto_destino;
-    date_time_set $hora_agendada_chegada;
-    date_time_set $hora_agendada_saida;
-    Aeronave $Aviao_esperado;
-    frequencia $Frequencia_voo;
-    
+public function __construct($codigo_f,$Aerop_origem_f,$Aerop_destino_f,$Hora_agen_chegada_f,$Hora_agen_saida_f,$Aviao_esperado_f,$frequencia_voo_f){
+    $this->codigo = $codigo_f;
+    $this->Aeroporto_origem = $Aerop_origem_f;
+    $this->Aeroporto_destino = $Aerop_destino_f;
+    $this->hora_agendada_chegada = $Hora_agen_chegada_f;
+    $this->hora_agendada_saida = $Hora_agen_saida_f;
+    $this->Aviao_esperado = $Aviao_esperado_f;
+    $this->Frequencia_voo = $frequencia_voo_f;
+    self::$historico_planejado[] = $this;
 }
 
-voo_planejado($codigo_f,$Aerop_origem_f,$Aerop_destino_f,$Hora_agen_chegada_f,$Hora_agen_saida_f,$Aviao_esperado_f,$frequencia_voo_f){
-
+public function validar_codigo($codigo){
+//Codigo composto por 2 letras e 4 numeros
+    $letras = substr($codigo,0,2);
+    $numeros = substr($codigo,2,4);
+    if (ctype_alpha($letras) && ctype_digit($numeros)){
+        return true;
+    }else{
+        return false;
+    }
 }
 
-validar_codigo(){
-
+public function get_frequencia_voo(){
+    return $this->Frequencia_voo;
 }
 
-get_frquencia_voo(){
-
+public function get_origem(){
+    return $this->Aeroporto_origem;
 }
 
-get_origem(){
-
+public function get_destino(){
+    return $this->Aeroporto_destino;
 }
 
-get_destino(){
-
+public function get_hora_agenda_chegada(){
+    return $this->hora_agendada_chegada;
 }
 
-get_hora_agenda_chegada(){
-
+public function get_hora_agenda_saida(){
+    return $this->hora_agendada_saida;
 }
 
-get_hora_agenda_saida(){
-
+public function get_aviao_marcado(){
+    return $this->Aviao_esperado;
 }
 
-get_aviao_marcado(){
-
+public function set_frequencia(){
+    return $this->Frequencia_voo;
 }
 
-colocar_na_agenda(){
-
+public function set_aviao_esp(){
+    return $this->Aviao_esperado;
 }
 
-set_frequencia(){
-
+public function set_hora_cheg_agend(){
+    return $this->hora_agendada_chegada;
 }
 
-set_aviao_esp(){
-
-}
-
-set_hora_cheg_agend(){
-
-}
-
-set_hora_said_agend(){
-
+public function set_hora_said_agend(){
+    return $this->hora_agendada_saida;
 }
 
 
