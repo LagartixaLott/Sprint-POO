5f913cf3d3312d730a72ce90267f1e9bda541b97
itzvini <viniciusmc.2109@gmail.com>
2023-04-08 15:51:38 -0300

    changes

--- testcases.php
+++ testcases.php
@@ -7,11 +7,14 @@ include_once("Aeroporto.php");
 
 $saida = new DateTime("2022-10-11 16:45:32");
 $chegada = new DateTime("2022-10-11 18:45:32");
+$novasaida = new DateTime("2022-10-11 18:35:32");
+$novachegada = new DateTime("2022-10-11 20:35:32");
 //Test Cases para a classe CompanhiaAerea
 $companhia = new CompanhiaAerea("Gol", "Gol Linhas Aereas", "123", "15.488.222/0001-72", "GL");
 echo  $companhia->get_razao()."\n";
 //Test Cases para a classe Aeronave
 $aeronave = new Aeronave("Boeing", "A-800", 186, 23600, "PR-GUO", $companhia);
+$nova_aeronave = new Aeronave("AeroBus", "M-650", 186, 23600, "PR-GUO", $companhia);
 echo $aeronave->get_companhia_aerea()->get_nome()."\n";
 
 //Test Cases para a classe Aeroporto
@@ -24,10 +27,11 @@ $voo_planejado = new VooPlanejado("GL1234", $congonhas, $teresina, $chegada, $sa
 echo $voo_planejado->get_frequencia_voo()."\n";
 
 //Test Cases para a classe VooDecolado
-$voo_decolado = new VooDecolado($voo_planejado, $saida, $chegada, $aeronave);
+$voo_decolado = new VooDecolado($voo_planejado, $novasaida, $novachegada, $nova_aeronave);
 echo $voo_decolado->get_aviao_voo()->get_companhia_aerea()->get_nome()."\n";
-
 echo $voo_planejado->get_hist_planejado();
+echo $voo_decolado->get_hist_executado();
+
 
 
 ?>
