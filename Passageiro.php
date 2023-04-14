4982c4052cfc0437deaf16b47c02f67961482f2c
Lott <127571086+LagartixaLott@users.noreply.github.com>
2023-04-14 17:31:59 -0300

    Add files via upload

---
+++ Passageiro.php
@@ -0,0 +1,43 @@
+<?php
+
+class Passageiro{
+   protected $nome_passageiro;
+    protected $sobrenome_passageiro;
+    protected $documento_passageiro;
+    protected $numero_bagagens;
+    
+    
+
+
+public function __construct($nome_p, $sobrenome_p,$documento_p){
+    $this->set_nome_passeiro($nome_p);
+    $this->set_sobrenome_passageiro($sobrenome_p);
+    $this->set_documento_passageiro($documento_p);
+
+}
+public function set_nome_passeiro($nome_p){
+    $this->nome_passageiro = $nome_p;
+}
+public function set_sobrenome_passageiro($sobrenome_p){
+    $this->sobrenome_passageiro = $sobrenome_p;
+}
+public function set_documento_passageiro($documento_p){
+    $this->documento_passageiro = $documento_p;
+}
+public function set_numero_bagagens($numero_bagagens_f){
+    $this->numero_bagagens = $numero_bagagens_f;
+}
+public function get_nome_passageiro(){
+    return $this->nome_passageiro;
+}
+public function get_sobrenome_passageiro(){
+    return $this->sobrenome_passageiro;
+}
+public function get_documento_passageiro(){
+    return $this->documento_passageiro;
+}
+public function get_nbagagens(){
+    return $this->numero_bagagens;
+}
+}
+    
